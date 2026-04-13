7f09208f5ddb303eee1543290167e6e538f4b32f
Ansarisjd <ansarisjdmohd3072@gmail.com>
2026-04-13 13:30:34 +0530

    Second Commit

--- ATM_Withdrawal_System.py
+++ ATM_Withdrawal_System.py
@@ -5,16 +5,17 @@ def atm_withdrawal(withdrwal_amount):
        print("Invalid amount. Please enter a positive amount.")
        return False
     
-    elif withdrwal_amount % 500 != 0:
+    if withdrwal_amount % 500 != 0:
        print("Invalid amount. Please enter a multiple of 500.")
        return False
-    elif withdrwal_amount > currentBalance:
+    if withdrwal_amount > currentBalance:
        print("Insufficient balance. Please enter amount less than or equal to your current balance.")
        return False 
-    else:
-       currentBalance -= withdrwal_amount
-       print(f"You have withdrwan amount {withdrwal_amount} successfully. \n Your current balance is:{currentBalance} \n Thank you for using our ATM service.")
-       
+
+    currentBalance -= withdrwal_amount
+    print(f"You have withdrwan amount {withdrwal_amount} successfully. \n Your current balance is:{currentBalance} \n Thank you for using our ATM service." )
+    return True 
 
 amount = int(input("Enter the amount you want to withdrawn"))
 result = atm_withdrawal(amount)
+print("Return:" , result)
